28cc5c90ea84980ea5bb9beebd60b397fb65cdde
Marken <27118779+Marken2808@users.noreply.github.com>
2020-10-21 18:09:20 +0100

    1

--- 1p_201020/src/TriangleTestingProgram/Triangle.java
+++ 1p_201020/src/TriangleTestingProgram/Triangle.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package practical1;
+package TriangleTestingProgram;
 
 import java.util.ArrayList;
 import java.util.Arrays;
